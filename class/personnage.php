e582fcf3d26341d106046a545b9daa8c233f038d
DidierColombbus <100928062+DidierColombbus@users.noreply.github.com>
2022-06-30 10:27:49 +0200

    Update personnage.php

---
+++ class/personnage.php
@@ -0,0 +1,17 @@
+<?php   
+    // Pour créer une class, on va utiliser le mot-clé class suivi du nom de notre classe, avec une majuscule. Le nom du fichier, lorsqu'il contient une class doit prendre une majuscule. Attention un fichier de class ne contient que les informations relatives à la classe à l'intérieur, l'affichage se fera sur une autre page.
+
+    class Personnage
+    {
+        // on définit des caractéristiques de nos personnages grâce à des propriétés. 
+        protected $vie = 80;
+        public $atk = 20;
+        // c'est sûrement par convention et pour s'y retrouver dans le code qu'on déclare notre propriété à cet endroit (on pourrait définir le nom directement dans la page affichage.php mais c'est fortement déconseillé).
+
+
+
+
+
+    }
+
+?>
